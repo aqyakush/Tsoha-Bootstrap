df21c6bc519e6ac73686db6a9f3feaec991e1c33
aqyakush <anton.yakushonok@gmail.com>
2016-09-19 11:11:08 +0300

    dokumentaatio päivitetty, lisätty tietokantataulujen pystytyslauseet, postolauseet, lisäyslauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,8 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO ASIAKAS (atunnus, nimi, password, lennot, syntymaaika) VALUES ('1','Matti','Matti123','AY2000','01.01.91');
+--tuote taulun testidata--
+INSERT INTO TUOTE (ttunnus,nimi,hinta,kuvas) VALUES ('1','Iphone 7','999.99','älypuhelin, uusi kamera, vesitiivis ja blablabla');
+--tilaus taulun testidata--
+INSERT INTO TILAUS(otunnus, atunnus, ttunnus, lento) VALUES ('1','1','1','AY200');
+--toiveet taulun testidata--
+INSERT INTO TOIVEET(atunnus, lento, toive) VALUES ('1','AY200','haluisin istua ikkunan vieressa');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,28 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE ASIAKAS(
+    atunnus SERIAL PRIMARY KEY,
+    nimi varchar(40) NOT NULL,
+    password varchar(50) NOT NULL,
+    Lennot varchar(20) NOT NULL,
+    syntymaaika varchar(8)
+);
+
+CREATE TABLE TUOTE(
+    ttunnus SERIAL PRIMARY KEY,
+    kuva BYTEA,
+    nimi varchar(40) NOT NULL,
+    hinta DECIMAL(10,2) NOT NULL,
+    kuvas varchar(300)
+);
+
+CREATE TABLE TILAUS(
+    otunnus SERIAL PRIMARY KEY,
+    atunnus INTEGER REFERENCES asiakas(atunnus),
+    ttunnus INTEGER REFERENCES tuote(ttunnus),
+    lento varchar(20) NOT NULL
+);
+CREATE TABLE TOIVEET(
+    atunnus INTEGER REFERENCES asiakas(atunnus),
+    lento varchar(20) NOT NULL,
+    toive varchar(300) NOT NULL
+);
