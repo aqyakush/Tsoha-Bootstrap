7e21d43908e0a53e2af2f499b51c4d937b0cd2f3
aqyakush <anton.yakushonok@gmail.com>
2016-10-20 19:12:20 +0300

    Dokumentaatio päivitetty ja test tables korjattu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,10 +2,12 @@
 --Asiakas taulus testidata--
 INSERT INTO ASIAKAS ( atunnus ,nimi, salasana, oikeuksia) VALUES ('1','Admin','Admin','0');
 INSERT INTO ASIAKAS ( atunnus ,nimi, salasana, oikeuksia) VALUES ('2','User1','User1','1');
-INSERT INTO ASIAKAS ( atunnus ,nimi, salasana, oikeuksia) VALUES ('3','User2','User2','1');
 --tuote taulun testidata--
---INSERT INTO TUOTE (ttunnus,nimi,hinta,kuvaus) VALUES ('1','Iphone 7','999.99','älypuhelin, uusi kamera, vesitiivis ja blablabla');
---INSERT INTO TUOTE (ttunnus,nimi,hinta,kuvaus) VALUES ('2','Kamera','123.45','kamera jolla pystyy ottaa kuvia');
+INSERT INTO TUOTE (nimi,hinta,kuvaus) VALUES ('Iphony','999.99','älypuhelin, uusi kamera, vesitiivis ja blablabla');
+INSERT INTO TUOTE (nimi,hinta,kuvaus) VALUES ('Kamera','123.45','kamera jolla pystyy ottaa kuvia');
+INSERT INTO TUOTE (nimi,hinta,kuvaus) VALUES ('Hajuvesi','40.00','Hyvää tuoksuinen harjuvesi, 40ml');
+INSERT INTO TUOTE (nimi,hinta,kuvaus) VALUES ('Suklaa','4.99','Vazzer sinen 100g');
+INSERT INTO TUOTE (nimi,hinta,kuvaus) VALUES ('Tietokone','799.00','Hyvää ja nopea tietokone');
 --tilaus taulun testidata--
 --INSERT INTO TILAUS(otunnus, atunnus, lento) VALUES ('1','1','AY200');
 --toiveet taulun testidata--
